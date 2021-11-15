5795a2a85157a4c4f4236d603547bb59a343d50e
Massawe14 <ramadhanimassawe14@gmail.com>
2021-11-15 09:31:10 +0300

    fixing footer

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,5 @@
   <footer class="main-footer">
-    <strong>Copyright &copy; 2021 <a href="https://necta.go.tz/">Necta</a>.</strong>
+    <strong>Copyright &copy; 2021 <a href="https://www.imperialinnovations.co.tz/">Imperial Innovations</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0.0
